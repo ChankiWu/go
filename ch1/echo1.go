5e28cd34e002ea08a2d92a2b7df4139681256be4
chankiwu <qiancw1@uci.edu>
2020-05-28 22:08:53 -0700

    Finish echo2 with range.

--- ch1/echo1.go
+++ ch1/echo1.go
@@ -8,13 +8,28 @@ import (
 )
 
 func main() {
+
+	// 等价声明
+
+	/* 	s := ""
+	var s string
+	var s = ""
+	var s string = "" */
+
 	var s, sep string
 
 	// os.Args的第一个元素：os.Args[0]，是命令本身的名字
 	// s[m:n]形式的切片表达式，产生从第m个元素到第n-1个元素的切片
 
-	for i := 1; i < len(os.Args); i++ {
-		s += sep + os.Args[i]
+	// 1.
+	// for i := 1; i < len(os.Args); i++ {
+	// 	s += sep + os.Args[i]
+	// 	sep = " "
+	// }
+
+	// 2.
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
 		sep = " "
 	}
 

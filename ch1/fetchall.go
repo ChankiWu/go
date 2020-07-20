dcba276e92407b8e489080666e955f8a59fc0a1a
ChankiWu <254152970@qq.com>
2020-07-20 17:36:29 +0800

    goroutine fetchall ch1.6

---
+++ ch1/fetchall.go
@@ -0,0 +1,55 @@
+// Fetchall fetches URLs in parallel and reports their times and sizes.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start a goroutine
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // receive from channel ch
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // send to channel ch
+		return
+	}
+	
+	// io.Copy会把响应的Body内容拷贝到ioutil.Discard输出流中
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close() // don't leak resources
+
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs  %7d  %s", secs, nbytes, url)
+}
+
+/*
+output:
+
+go run ch1\fetchall.go http://gopl.io https://godoc.org https://www.baidu.com/
+0.57s      227  https://www.baidu.com/
+1.26s     7544  https://godoc.org
+2.30s     4154  http://gopl.io
+2.30s elapsed
+
+*/

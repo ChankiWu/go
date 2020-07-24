c6fb21a8efa324692c339a67f54fa457adc697ce
ChankiWu <254152970@qq.com>
2020-07-24 10:42:38 +0800

    Ch6 encapsulation

---
+++ ch6/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+type Point struct{ X, Y float64 }
+
+// traditional function
+func Distance(p, q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+// same thing, but as a method of the Point type
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(q.X-p.X, q.Y-p.Y)
+}
+
+type Counter struct { n int }
+func (c *Counter) N() int     { return c.n }
+func (c *Counter) Increment() { c.n++ }
+func (c *Counter) Reset()     { c.n = 0 }
+
+func main() {
+	var c Counter
+	c.Increment()
+	fmt.Println(c.N())	// 1
+}
+
